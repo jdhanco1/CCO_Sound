c3768edf0225131c2d6671bf779a270c9baa5ec5
jdhanco1 <jdhanco1@gmail.com>
2026-04-07 10:18:18 -0500

    Add files via upload

---
+++ cms/src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    /**
+     * An asynchronous register function that runs before
+     * your application is initialized.
+     */
+    register( /* { strapi } */) { },
+    /**
+     * An asynchronous bootstrap function that runs after
+     * all services, hooks and middlewares are loaded.
+     */
+    bootstrap( /* { strapi } */) { },
+};
+//# sourceMappingURL=index.js.map
