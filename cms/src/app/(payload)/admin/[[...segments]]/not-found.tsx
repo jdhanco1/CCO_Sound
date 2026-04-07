1730e261556ea60eb61f2f4c0625dca02ca18b7a
jdhanco1 <jdhanco1@gmail.com>
2026-04-07 12:12:42 -0500

    docker

--- cms/src/app/(payload)/admin/[[...segments]]/not-found.tsx
+++ cms/src/app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -1,3 +1,19 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-export { GET } from '@payloadcms/next/not-found'
+import config from '@payload-config'
+import { NotFoundPage, generatePageMetadata } from '@payloadcms/next/views'
+import type { Metadata } from 'next'
+import { importMap } from '../../importMap'
+
+type Args = {
+  params: Promise<{ segments: string[] }>
+  searchParams: Promise<Record<string, string | string[]>>
+}
+
+export const generateMetadata = async ({ params, searchParams }: Args): Promise<Metadata> =>
+  generatePageMetadata({ config, params, searchParams })
+
+const NotFound = ({ params, searchParams }: Args) =>
+  NotFoundPage({ config, importMap, params, searchParams })
+
+export default NotFound
